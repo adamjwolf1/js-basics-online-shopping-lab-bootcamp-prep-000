ae1a2f1a8cf46def3ecd4aa2d2020fd252d69140
Adam Wolf <adamjwolf1@gmail.com>
2019-02-19 19:13:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function total() {
 function removeFromCart(item) {
   
   
-  for(var i = 0; i < cart.length; i++) {
+  for (var i = 0; i < cart.length; i++) {
     if(i === cart[i].itemName) {
       cart.splice(i, 1);
       return cart;
