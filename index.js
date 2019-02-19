9d48f6a6ce1e00a3e3e424015a7d44e64e2a4e8a
Adam Wolf <adamjwolf1@gmail.com>
2019-02-19 19:09:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ function removeFromCart(item) {
   for(var i = 0; i< cart.length; i++) {
     if(i === cart[i].itemName) {
       cart.splice(i, 1);
-      return cart
+      return cart;
     }
   }
   if (item != cart[item]) {
-    return "That item is not in your cart."
+    return "That item is not in your cart.";
   }
 }
 
