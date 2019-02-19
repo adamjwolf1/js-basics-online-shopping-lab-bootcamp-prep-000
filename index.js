32dd5e16d166008e2ce4571224eef9a709146da7
Adam Wolf <adamjwolf1@gmail.com>
2019-02-19 19:10:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,15 +41,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-  for(var i = 0; i< cart.length; i++) {
-    if(i === cart[i].itemName) {
-      cart.splice(i, 1);
-      return cart;
-    }
-  }
-  if (item != cart[item]) {
-    return "That item is not in your cart.";
-  }
+
 }
 
 function placeOrder(cardNumber) {
